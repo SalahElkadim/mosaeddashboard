65ccb6fb8739da3cd218f60b40337a80d01c03eb
unknown <khaledgaber494@gmail.com>
2026-06-06 17:44:11 +0300

    initial commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-});
+const baseURL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
+
+const api = axios.create({ baseURL });
 
 // بيضيف الـ token تلقائياً لكل request
 api.interceptors.request.use((config) => {
